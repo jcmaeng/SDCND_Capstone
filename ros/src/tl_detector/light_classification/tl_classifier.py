41a6b881523ab3c0f8eb8be70ccd7863fc8a36d8
hapuhundre <mangertim@163.com>
2018-06-07 12:09:58 +0800

    update t1_classifier

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,25 @@
 from styx_msgs.msg import TrafficLight
 
+import os
+
+
 class TLClassifier(object):
     def __init__(self):
         #TODO load classifier
-        pass
+        # pass
+        num_classes = 3
+        pwd = os.path.dirname(os.path.realpath(__file__))
+        model_path = os.path.join(pwd, '')
+        labeltxt_path = os.path.join(pwd, '')
+
+        self.img = None
+        self.category_dict =  { 1:{'name':'red', 'id':1}, 
+                                2:{'name':'yellow', 'id':2}, 
+                                3:{'name':'green', 'id':3}}
+        
+
+
+
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -17,4 +33,13 @@ class TLClassifier(object):
         """
         #TODO implement light color prediction
         
-        return TrafficLight.UNKNOWN
+        traffic_light = TrafficLight.UNKNOWN
+        prediction
+        if prediction == 'red':
+            traffic_light = TrafficLight.RED
+        elif prediction == 'green':
+            traffic_light = TrafficLight.GREEN
+        elif prediction == 'YELLOW':
+            traffic_light = TrafficLight.YELLOW'
+        
+        return traffic_light
