c40240d2ac08afc4d8bfa5593cacb83c7f08850f
hapuhundre <mangertim@163.com>
2018-06-08 14:22:46 +0800

    update classification

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -17,6 +17,7 @@ class TLClassifier(object):
                                 2:{'name':'yellow', 'id':2}, 
                                 3:{'name':'green', 'id':3}}
         
+        self.sess = tf.Session()
 
 
 
@@ -34,12 +35,26 @@ class TLClassifier(object):
         #TODO implement light color prediction
         
         traffic_light = TrafficLight.UNKNOWN
-        prediction
-        if prediction == 'red':
-            traffic_light = TrafficLight.RED
-        elif prediction == 'green':
-            traffic_light = TrafficLight.GREEN
-        elif prediction == 'YELLOW':
-            traffic_light = TrafficLight.YELLOW
+        min_threshold = 0.5
+
+        with self.d_graph.as_default():
+            expanded_img = np.expand_dims(image, axis=0)
+            (boxes, scores, classes, num) = self.sess.run([self.detection_boxes,self.detection_scores, 
+                                                           self.detection_classes,self.detection_num],
+                                                           feed_dict={self.img_tensor: expanded_img})
+            boxes = np.squeeze(boxes)
+            classes = np.squeeze(classes).astype(np.int32)
+            scores = np.squeeze(scores)
+
+            for idx in range(boxes.shape[0]):
+                if scores[idx] > min_threshold:
+                    prediction = self.category_dict[classes[idx]]['name']
+              
+                    if prediction == 'red':
+                        traffic_light = TrafficLight.RED
+                    elif prediction == 'green':
+                        traffic_light = TrafficLight.GREEN
+                    elif prediction == 'yellow':
+                        traffic_light = TrafficLight.YELLOW
         
         return traffic_light
